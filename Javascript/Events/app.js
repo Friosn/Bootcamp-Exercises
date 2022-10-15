a84685f3c81e3af1631b112a8736a26dc27b6b74
Friosn <franz.rios.nino@gmail.com>
2022-10-15 17:20:48 +0200

    Events 1.1

---
+++ Javascript/Events/app.js
@@ -0,0 +1,8 @@
+const addBtnLikeABoss = document.querySelector("#btnToClick");
+addBtnLikeABoss.textContent = "I am a button";
+
+addBtnLikeABoss.addEventListener("click", () => {
+  console.log(
+    "I don't know what information do I have to print here, but here I come :)"
+  );
+});
