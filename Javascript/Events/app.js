9618eefe42f187253e842b8fbbae01c75d2aa5e1
Friosn <franz.rios.nino@gmail.com>
2022-10-15 17:56:25 +0200

    Events 1.2

--- Javascript/Events/app.js
+++ Javascript/Events/app.js
@@ -1,3 +1,5 @@
+//1.1 -----------
+
 const addBtnLikeABoss = document.querySelector("#btnToClick");
 addBtnLikeABoss.textContent = "I am a button";
 
@@ -6,3 +8,18 @@ addBtnLikeABoss.addEventListener("click", () => {
     "I don't know what information do I have to print here, but here I come :)"
   );
 });
+
+//1.2---------------------
+
+const addingAnother = document.querySelector(".focus");
+
+addingAnother.addEventListener("focus", (value) => {
+  console.log(value.target.value);
+});
+
+/* 
+const handleFocus = (evt) => console.log(evt.target.value);
+let input = document.querySelector('input[class="focus"]');
+
+input.addEventListener("focus", handleFocus);
+ */
