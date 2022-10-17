874cdbf3b9911df083be1602de3a8b8debe92683
Friosn <franz.rios.nino@gmail.com>
2022-10-17 16:51:04 +0200

    Iteration 8 Bonus ES6

--- Javascript/ES6/6-forward/app.js
+++ Javascript/ES6/6-forward/app.js
@@ -85,3 +85,27 @@ console.log(sumAprobedOnes);
 
 const averageSocres = sumScores / exams.length;
 console.log(averageSocres);
+
+//ITERATION#8----------BONUS---------
+
+const videogames = [
+  { name: "Final Fantasy VII", genders: ["RPG"], score: 9.5 },
+  { name: "Assasins Creed Valhala", genders: ["Aventura", "RPG"], score: 4.5 },
+  { name: "The last of Us 2", genders: ["Acción", "Aventura"], score: 9.8 },
+  { name: "Super Mario Bros", genders: ["Plataforma"], score: 8.5 },
+  { name: "Genshin Impact", genders: ["RPG", "Aventura"], score: 7.5 },
+  {
+    name: "Legend of Zelda: Breath of the wild",
+    genders: ["RPG", "La cosa más puto bonita que he visto nunca"],
+    score: 10,
+  },
+];
+
+const rpgGames = videogames.filter((game) => game.genders.includes("RPG"));
+
+const scoreSum = rpgGames.reduce(
+  (accumulator, game) => accumulator + game.score,
+  0
+);
+const averageGameScore = scoreSum / rpgGames.length;
+console.log(averageGameScore);
