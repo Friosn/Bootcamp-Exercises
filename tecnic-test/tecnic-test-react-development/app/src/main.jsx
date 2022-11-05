68d7fff066307144ce847fb95d2c850ee05814f2
Friosn <franz.rios.nino@gmail.com>
2022-11-05 14:15:27 +0100

    Routing and pages

--- tecnic-test/tecnic-test-react-development/app/src/main.jsx
+++ tecnic-test/tecnic-test-react-development/app/src/main.jsx
@@ -2,11 +2,26 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import About from './pages/About';
+import NotFound from './pages/404';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="series" element={<Series />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
